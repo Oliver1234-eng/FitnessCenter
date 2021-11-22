d007716e398252fe7b73a90f98d148eadf2a9950
Oliver1234-eng <oliver.kojic22@gmail.com>
2021-11-22 17:50:35 +0100

    Dodati exceptioni za sve tipove korisnika, i u folder Resursi ubaceni podaci - txt fajlovi sa korisnicima (kopirani iz projekta radjenog uporedo sa vezbama, kada se radila konzolna aplikacija)

---
+++ MojiIzuzeci/InstruktorNePostojiException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR12_2020_POP2021.MojiIzuzeci
+{
+    public class InstruktorNePostojiException : Exception
+    {
+        public InstruktorNePostojiException() : base()
+        {
+
+        }
+
+        public InstruktorNePostojiException(string poruka) : base(poruka)
+        {
+
+        }
+
+        public InstruktorNePostojiException(string poruka, Exception ex) : base(poruka, ex)
+        {
+
+        }
+    }
+}
---
+++ MojiIzuzeci/KorisnikNePostojiException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR12_2020_POP2021.MojiIzuzeci
+{
+    public class KorisnikNePostojiException : Exception
+    {
+        public KorisnikNePostojiException() : base()
+        {
+
+        }
+
+        public KorisnikNePostojiException(string poruka) : base(poruka)
+        {
+
+        }
+
+        public KorisnikNePostojiException(string poruka, Exception ex) : base(poruka, ex)
+        {
+
+        }
+    }
+}
